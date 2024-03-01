98a16b997e546f8c6fe21f23d8627db39aff4933
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-03-01 10:08:27 +0100

    Perceptron bug fixes

--- perceptron.py
+++ perceptron.py
@@ -4,9 +4,9 @@ import numpy as np
 class Perceptron:
     def __init__(self, training_set):
         self.training_data = training_set
-        num_features = len(training_set[0][0])
-        num_results = len(training_set[0][1])
-        self.weights = np.random.randn(num_results, num_features + 1)
+        self.num_features = len(training_set[0][0])
+        self.num_results = len(training_set[0][1])
+        self.weights = np.random.randn(self.num_results, self.num_features + 1)
         self.learning_rate = 0.3
         self.epochs = 1000
 
@@ -22,7 +22,7 @@ class Perceptron:
                 product = np.dot(self.weights, actual_input)
                 actual_output = self.signum_function(product)
                 error = desired_output - actual_output
-                self.weights += np.dot(self.learning_rate * error.reshape([2, 1]), actual_input.reshape([1, 4]))
+                self.weights += np.dot(self.learning_rate * error.reshape([self.num_results, 1]), actual_input.reshape([1, self.num_features + 1]))
 
     def run(self, net_input):
         actual_input = np.insert(net_input, 0, -1.0)
@@ -34,11 +34,4 @@ class Perceptron:
         print("Output vector with applied sign function is: ", actual_output)
 
 
-if __name__ == "__main__":
-    training_set = [([0, 0], [0]), ([0, 1], [1]), ([1, 0], [1]), ([1, 1], [1])]
-    perceptron = Perceptron(training_set)
-    perceptron.learning()
-    perceptron.run([0, 0])
-    perceptron.run([0, 1])
-    perceptron.run([1, 0])
-    perceptron.run([1, 1])
+    

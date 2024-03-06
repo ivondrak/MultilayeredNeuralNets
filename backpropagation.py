2519543f1359a4d674d7c7ce36fb45f46a6f6d76
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-03-06 15:19:24 +0100

    Backpropagation with bias adaptation implemented

--- backpropagation.py
+++ backpropagation.py
@@ -7,12 +7,12 @@ class BackPropagation:
         self.weights = []
         for i in range(len(topology)-1):
             self.weights.append(np.random.randn(topology[i + 1], topology[i]))
-        #self.biases = []
-        #for i in range(1, len(topology)):
-        #    self.biases.append(np.random.randn(1, topology[i]))
-        #self.slopes = []
-        #for i in range(1, len(topology)):
-        #    self.biases.append(np.random.randn(1, topology[i]))
+        self.biases = []
+        for i in range(1, len(topology)):
+            self.biases.append(np.random.randn(topology[i], 1))
+        self.deltas_biases = []
+        for i in range(len(topology) - 1):
+            self.deltas_biases.append(np.zeros((topology[i + 1], 1)))
         self.activations = []
         for i in range(len(topology)):
             self.activations.append(np.zeros((topology[i], 1)))
@@ -49,8 +49,14 @@ class BackPropagation:
     def feed_forward(self, net_input):
         actual_input = np.array(net_input)
         self.activations[0] = actual_input.reshape(len(actual_input), 1)
+        #for i in range(1, self.num_layers):
+        #    self.activations[i] = self.sigmoid_function(np.dot(self.weights[i - 1], self.activations[i - 1]))
         for i in range(1, self.num_layers):
-            self.activations[i] = self.sigmoid_function(np.dot(self.weights[i - 1], self.activations[i - 1]))
+            w = self.weights[i - 1]
+            b = self.biases[i - 1]
+            x = self.activations[i - 1]
+            z = np.dot(w, x)
+            self.activations[i] = self.sigmoid_function(z)
         self.output_activation = np.around(self.activations[-1], decimals=2)
 
     def calculate_errors(self, desired_output):
@@ -62,15 +68,22 @@ class BackPropagation:
         self.errors[-1] = self.output_error * self.sigmoid_derivative(self.activations[-1])
         self.gradients[-1] = np.dot(self.errors[-1], self.activations[-2].T)
         self.deltas[-1] = self.learning_rate * self.gradients[-1]
+        self.deltas_biases[-1] = self.learning_rate * self.errors[-1]
         for i in range(self.num_layers - 2, 0, -1):
-            self.errors[i-1] = np.dot(self.weights[i].T, self.errors[i] * self.sigmoid_derivative(
+            self.errors[i - 1] = np.dot(self.weights[i].T, self.errors[i] * self.sigmoid_derivative(
                 self.activations[i + 1]))
             self.gradients[i] = np.dot(self.errors[i], self.activations[i].T)
             self.deltas[i] = self.learning_rate * self.gradients[i]
+        self.deltas_biases[-1] = self.learning_rate * self.errors[-1]
+        for i in range(self.num_layers - 2, 0, -1):
+            self.deltas_biases[i - 1] = self.learning_rate * self.errors[i - 1] * -1 #dy/d bias = -1
+
 
     def update_weights(self):
         for i in range(len(self.weights)):
             self.weights[i] -= self.deltas[i]
+        for i in range(len(self.biases)):
+            self.biases[i] -= self.deltas_biases[i]
 
     def run(self, input_data):
         self.feed_forward(input_data)

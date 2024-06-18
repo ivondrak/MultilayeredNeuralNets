7631a48a661f504ffab511201a28f618503a2fb8
Ivo Vondrak <ivo.vondrak@gmail.com>
2024-06-18 18:43:40 +0200

    Update backpropagation implementation to use ReLU activation function

--- backpropagation.py
+++ backpropagation.py
@@ -1,8 +1,107 @@
 import numpy as np
 
+# Weights only adaptation
+class GenericBackPropagation:
 
+    def __init__(self, training_set, topology, learning_rates, epochs):
+        self.training_data = training_set
+        self.weights = []
+        for i in range(len(topology)-1):
+            self.weights.append(np.random.rand(topology[i + 1], topology[i]))
+        self.activations = []
+        for i in range(len(topology)):
+            self.activations.append(np.zeros((topology[i], 1)))
+        self.errors = []
+        for i in range(len(topology)):
+            self.errors.append(np.zeros((topology[i], 1)))
+        self.deltas_weights = []
+        for i in range(len(topology)-1):
+            self.deltas_weights.append(np.zeros((topology[i+1], topology[i])))
+        self.gradients = []
+        for i in range(len(topology)-1):
+            self.gradients.append(np.zeros((topology[i+1], topology[i])))
+        self.num_layers = len(topology)
+        self.output_error = np.zeros((topology[-1], 1))
+        self.output_activation = np.zeros((topology[-1], 1))
+        self.learning_rates = learning_rates
+        self.epochs = epochs
+        self.history = []
 
-class BackPropagation:
+    def activation_function(self, z):
+        return 1 / (1 + np.exp(-1 * z))
+
+    def activation_derivative(self, y):
+        return y * (1 - y)
+
+    def backpropagation(self):
+        self.history = []
+        for epoch in range(self.epochs):
+            for data in self.training_data:
+                self.feed_forward(data[0])
+                self.calculate_errors(data[1])
+                self.calculate_gradients()
+                self.update_neural_net()
+            mean_squared_error = self.calculate_mean_squared_error()
+            self.history.append(mean_squared_error)
+
+    def feed_forward(self, net_input):
+        actual_input = np.array(net_input)
+        self.activations[0] = actual_input.reshape(len(actual_input), 1)
+        for i in range(1, self.num_layers):
+            w = self.weights[i - 1]
+            x = self.activations[i - 1]
+            z = w @ x
+            self.activations[i] = self.activation_function(z)
+        self.output_activation = np.around(self.activations[-1], decimals=2)
+
+    def calculate_errors(self, desired_output):
+        array_desired_output = np.array(desired_output)
+        array_desired_output = array_desired_output.reshape(len(desired_output), 1)
+        self.output_error =  self.activations[-1] - array_desired_output
+
+    def calculate_gradients(self):
+        self.errors[-1] = self.output_error
+        self.gradients[-1] = (self.errors[-1] * self.activation_derivative(self.activations[-1])) @ self.activations[-2].T
+        self.calculate_weights()
+
+    def calculate_weights(self):
+        self.deltas_weights[-1] = self.learning_rates[0] * self.gradients[-1]
+        for i in range(self.num_layers - 2, 0, -1):
+            self.errors[i] = self.weights[i].T @ self.errors[i + 1]
+            self.gradients[i - 1] = (self.errors[i] * self.activation_derivative(self.activations[i])) @ self.activations[i - 1].T
+            self.deltas_weights[i - 1] = self.learning_rates[0] * self.gradients[i - 1]
+
+    def update_neural_net(self):
+        for i in range(len(self.weights)):
+            self.weights[i] -= self.deltas_weights[i]
+
+    def run(self, input_data):
+        self.feed_forward(input_data)
+        return self.output_activation.flatten().tolist()
+    
+    def calculate_mean_squared_error(self):
+        total_error = 0
+        for data in self.training_data:
+            input_data = data[0]
+            desired_output = data[1]
+            actual_output = self.run(input_data)
+            error = np.sum((np.array(actual_output) - np.array(desired_output)) ** 2)
+            total_error += error
+        mean_squared_error = total_error / len(self.training_data)
+        return mean_squared_error
+
+    def calculate_max_error(self):
+        max_error = 0
+        for data in self.training_data:
+            input_data = data[0]
+            desired_output = data[1]
+            predicted_output = self.run(input_data)
+            error = np.max(np.abs(np.array(desired_output) - np.array(predicted_output)))
+            max_error = max(max_error, error)
+        return max_error  
+
+# Weights, biases, and slopes adaptation
+class BackPropagation (GenericBackPropagation):
     def __init__(self, training_set, topology, learning_rates, epochs):
         self.training_data = training_set
         self.weights = []
@@ -39,10 +138,10 @@ class BackPropagation:
         self.epochs = epochs
         self.history = []
 
-    def sigmoid_function(self, z, slope, bias):
+    def activation_function(self, z, slope, bias):
         return 1 / (1 + np.exp(-1 * slope * (z - bias)))
 
-    def sigmoid_derivative(self, y, slope):
+    def activation_derivative(self, y, slope):
         return slope * y * (1 - y)
 
     def slope_derivative(self, y, z, bias):
@@ -50,18 +149,6 @@ class BackPropagation:
 
     def bias_derivative(self, y, slope):
         return -1 * slope * y * (1 - y)
-
-
-    def backpropagation(self):
-        self.history = []
-        for epoch in range(self.epochs):
-            for data in self.training_data:
-                self.feed_forward(data[0])
-                self.calculate_errors(data[1])
-                self.calculate_gradients()
-                self.update_neural_net()
-            mean_squared_error = self.calculate_mean_squared_error()
-            self.history.append(mean_squared_error)
 
     def feed_forward(self, net_input):
         actual_input = np.array(net_input)
@@ -72,20 +159,12 @@ class BackPropagation:
             b = self.biases[i]
             x = self.activations[i - 1]
             z = w @ x
-            self.activations[i] = self.sigmoid_function(z, s, b)
+            self.activations[i] = self.activation_function(z, s, b)
         self.output_activation = np.around(self.activations[-1], decimals=2)
 
-    def calculate_errors(self, desired_output):
-        array_desired_output = np.array(desired_output)
-        array_desired_output = array_desired_output.reshape(len(desired_output), 1)
-        self.output_error =  self.activations[-1] - array_desired_output
-
     def calculate_gradients(self):
-        #self.errors[-1] = self.output_error * self.sigmoid_derivative(self.activations[-1])
-        # Weights gradients
         self.errors[-1] = self.output_error
-        # self.gradients[-1] = np.dot(self.errors[-1] * self.sigmoid_derivative(self.activations[-1], self.slopes[-1]), self.activations[-2].T)
-        self.gradients[-1] = (self.errors[-1] * self.sigmoid_derivative(self.activations[-1], self.slopes[-1])) @ self.activations[-2].T
+        self.gradients[-1] = (self.errors[-1] * self.activation_derivative(self.activations[-1], self.slopes[-1])) @ self.activations[-2].T
         self.calculate_weights()
         self.calculate_biases()
         self.calculate_slopes()
@@ -95,7 +174,7 @@ class BackPropagation:
         for i in range(self.num_layers - 2, 0, -1):
             self.errors[i] = self.weights[i].T @ self.errors[i + 1]
             # self.gradients[i - 1] = np.dot(self.errors[i] * self.sigmoid_derivative(self.activations[i], self.slopes[i]), self.activations[i - 1].T)
-            self.gradients[i - 1] = (self.errors[i] * self.sigmoid_derivative(self.activations[i], self.slopes[i])) @ self.activations[i - 1].T
+            self.gradients[i - 1] = (self.errors[i] * self.activation_derivative(self.activations[i], self.slopes[i])) @ self.activations[i - 1].T
             self.deltas_weights[i - 1] = self.learning_rates[0] * self.gradients[i - 1]
 
     def calculate_biases(self):
@@ -150,3 +229,38 @@ class BackPropagation:
             error = np.max(np.abs(np.array(desired_output) - np.array(predicted_output)))
             max_error = max(max_error, error)
         return max_error  
+
+# ReLU activation function    
+class ReLUBackPropagation(GenericBackPropagation):
+    
+    def __init__(self, training_set, topology, learning_rates, epochs):
+        self.training_data = training_set
+        self.weights = []
+        # He initialization
+        for i in range(len(topology)-1):
+            self.weights.append(np.random.randn(topology[i + 1], topology[i]) * np.sqrt(2.0/topology[i]))
+        self.activations = []
+        for i in range(len(topology)):
+            self.activations.append(np.zeros((topology[i], 1)))
+        self.errors = []
+        for i in range(len(topology)):
+            self.errors.append(np.zeros((topology[i], 1)))
+        self.deltas_weights = []
+        for i in range(len(topology)-1):
+            self.deltas_weights.append(np.zeros((topology[i+1], topology[i])))
+        self.gradients = []
+        for i in range(len(topology)-1):
+            self.gradients.append(np.zeros((topology[i+1], topology[i])))
+        self.num_layers = len(topology)
+        self.output_error = np.zeros((topology[-1], 1))
+        self.output_activation = np.zeros((topology[-1], 1))
+        self.learning_rates = learning_rates
+        self.epochs = epochs
+        self.history = []
+
+    def activation_function(self, z):
+        return np.maximum(0, z)
+    
+    def activation_derivative(self, y):
+        return np.where(y > 0, 1, 0)
+    
